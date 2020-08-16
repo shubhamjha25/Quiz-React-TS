4d667777c54477ce4d6f59169f194e7c9798203d
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-16 20:40:17 +0700

    Components & Basic Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import Questions from './components/Questions'
 
 function App() {
+
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>Quiz</h1>
+      <button onClick={startQuiz}>Start</button>
+      <p className="score">
+        Score : 
+      </p>
+      <p className="loading">
+        Loading Question . . . 
+      </p>
+      <Questions />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
